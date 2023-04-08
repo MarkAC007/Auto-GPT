d8bba64217c8616de2e41768a25ee227ddf54861
MarkAC007 <56160609+MarkAC007@users.noreply.github.com>
2023-04-08 09:41:29 +0100

    Update speak.py

--- scripts/speak.py
+++ scripts/speak.py
@@ -7,14 +7,14 @@ import gtts
 
 
 # TODO: Nicer names for these ids
-voices = ["ErXwobaYiN019PkySvjV", "EXAVITQu4vr4xnSDxMaL"]
+voices = ["ErXwobaYiN019PkySvjV", "EXAVITQu4vr4xnSDxMaL","21m00Tcm4TlvDq8ikWAM"]
 
 tts_headers = {
     "Content-Type": "application/json",
     "xi-api-key": cfg.elevenlabs_api_key
 }
 
-def eleven_labs_speech(text, voice_index=0):
+def eleven_labs_speech(text, voice_index=2):
     tts_url = "https://api.elevenlabs.io/v1/text-to-speech/{voice_id}".format(
         voice_id=voices[voice_index])
     formatted_message = {"text": text}
